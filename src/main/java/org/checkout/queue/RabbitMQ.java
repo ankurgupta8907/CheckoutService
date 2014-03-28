3c3e8c05cb63d4a460a9d9ee4c3eca1ea58c0c13
a.gupta <a.gupta@flipkart.com>
2014-03-28 18:19:11 +0530

    Commiting refactoring.

--- src/main/java/org/checkout/queue/RabbitMQ.java
+++ src/main/java/org/checkout/queue/RabbitMQ.java
@@ -3,8 +3,6 @@ package org.checkout.queue;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
